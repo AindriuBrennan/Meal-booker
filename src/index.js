7a987aa4d7ed4d03a65296eb6132fe5fcaa71796
Aindriu <20079247@mail.wit.ie>
2019-11-05 09:45:55 +0000

    firebase and react connected by using the react context api to provide a firebase instance, this enables a firebase instace to be provided to the entire application, any component that might need to use firebase can access it via FirebaseContext

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import HomePage from './components/homePage';
-import Routes from './Routes';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import HomePage from "./components/homePage";
+import Routes from "./Routes";
+import Firebase, { FirebaseContext } from "./components/firebase";
 
 ReactDOM.render(
-    <Router>
-      <App />
-    </Router>,
-    document.getElementById('root')
-  );
-  
+  <Router>
+    <FirebaseContext.Provider value ={new Firebase()}>
+    <App />
+    </FirebaseContext.Provider>
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
